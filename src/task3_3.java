ba39cbf3d7dbe8a06aac396b4d8cae35d9d0ed05
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-28 23:04:50 +0900

    finish task3_3

---
+++ src/task3_3.java
@@ -0,0 +1,15 @@
+
+public class task3_3 {
+	public static void main(String[] args) {
+		int isHungry = 0;
+		String food = "リンゴ";
+		System.out.println("こんにちは");
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+			System.out.println(food + "をいただきます");
+		}
+		System.out.println("ごちそうさまでした");
+	}
+}
