3a6d666dd0a686f2a6ed969472e8a139fa26069c
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-28 23:12:19 +0900

    start task3_4

---
+++ src/task3_4.java
@@ -0,0 +1,11 @@
+
+public class task3_4 {
+	public static void main(String[] args) {
+		boolean tenki = true;
+		if (tenki == true) {
+			System.out.println("選択をします");
+			System.out.println("散歩に行きます");
+		} else
+			System.out.println("DVDを見ます");
+	}
+}
