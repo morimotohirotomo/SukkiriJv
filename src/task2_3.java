f84447cde4382ee12aee7daab446d1cde3a7171c
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-28 20:13:51 +0900

    finish task2_3

--- src/task2_3.java
+++ src/task2_3.java
@@ -1,6 +1,17 @@
-
+import java.util.Scanner;
 public class task2_3 {
 	public static void main(String[] args) {
-		
-	}
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("ようこそ占いの館へ");
+		System.out.println("あなたの名前を入力してください");
+		String name = scanner.next();
+		System.out.println("あなたの年齢を入力してください");
+		String ageString = scanner.next();
+		int age = Integer.parseInt(ageString);
+		int fortune = new java.util.Random().nextInt(4);
+		fortune++;
+		System.out.println("占いの結果が出ました!");
+		System.out.println(age + "歳の" + name + "さん、あなたの運気番号は" + fortune + "です");
+		System.out.println("1:大吉 2:中吉 3:吉 4:凶");
+;	}
 }
