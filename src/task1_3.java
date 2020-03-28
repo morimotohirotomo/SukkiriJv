d0923b36bcd739131fe2fdbd551fd1dac066ffba
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-28 18:00:51 +0900

    finish task1_3

---
+++ src/task1_3.java
@@ -0,0 +1,10 @@
+
+public class task1_3 {
+  public static void main(String[] args) {
+    boolean bool = true;
+    char name = '駆';
+    double pi = 3.14;
+    long number = 65447899654L;
+    String damage = "1234556ダメージ";
+  }
+}
