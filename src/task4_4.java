15bda253743ff29acda29e57f28ed0ccee4c2367
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-29 23:57:38 +0900

    finish task4_4

--- src/task4_4.java
+++ src/task4_4.java
@@ -2,5 +2,13 @@ import java.util.Scanner;
 public class task4_4 {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
+		int numbers[] = {3, 4, 9};
+		System.out.println("1桁の数字を入力してください");
+		int input = scanner.nextInt();
+		for (int n : numbers) {
+			if (n == input) {
+				System.out.println("アタリ!");
+			}
+		}
 	}
 }
