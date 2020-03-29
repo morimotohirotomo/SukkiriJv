e70a43573aca4f85b964e749619501949918c4e9
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-29 23:43:12 +0900

    finish task4_2_2

--- src/task4_2.java
+++ src/task4_2.java
@@ -1,6 +1,9 @@
 
 public class task4_2 {
 	public static void main(String[] args) {
-
+		int moneyList[] = {121902, 8302, 5510};
+		for (int i = 0; i < moneyList.length; i++) {
+			System.out.println(moneyList[i]);
+		}
 	}
 }
