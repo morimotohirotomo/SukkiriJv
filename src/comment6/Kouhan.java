eb2994063c2662716635383620866cb8002a4c34
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-31 15:45:26 +0900

    finish task6_1

---
+++ src/comment6/Kouhan.java
@@ -0,0 +1,10 @@
+package comment6;
+
+public class Kouhan {
+	public static void callDeae() {
+		System.out.println("えぇい、小癪な");
+	}
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん");
+	}
+}
---
+++ src/task6_1.java
@@ -0,0 +1,9 @@
+import comment6.Zenhan;
+public class task6_1 {
+	public static void main(String[] args) throws Exception {
+		Zenhan.doWarusa();
+		Zenhan.doTogame();
+		comment6.Kouhan.callDeae();
+		comment6.Kouhan.showMondokoro();
+	}
+}
