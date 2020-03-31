d649601f65df3e7aa0ad65ea00e40ae45886cc7c
morimotohirotomo <minamoyori19@outlook.jp>
2020-03-31 12:10:12 +0900

    finish task5_1

---
+++ src/task5_1.java
@@ -0,0 +1,14 @@
+
+public class task5_1 {
+	public static void main(String[] args) {
+		introduceOneself();
+	}
+
+	public static void introduceOneself() {
+		String name = "Jon";
+		int age = 19;
+		double height = 1.8;
+		char animal = '丑';
+		System.out.println("name:" + name + ", age:" + age + ", height" + height + ", char:" + animal);
+	}
+}
