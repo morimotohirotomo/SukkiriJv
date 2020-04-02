79634ad3fdbfb9f293b2d4c992a1a6759a83edc2
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-02 20:33:14 +0900

    finish 2_2

--- src/Pro/Base.java
+++ src/Pro/Base.java
@@ -4,6 +4,7 @@ public class Base {
 	public static void main(String[] arge) {
 		System.out.println("1足す2は" + 3);
 		System.out.println(5/2);
-		System.out.pritnln(5.0/2);
+		System.out.println(5.0/2);
+		System.out.println((double)13);
 	}
 }
