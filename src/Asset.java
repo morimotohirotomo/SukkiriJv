98e9f64b9bb78eb718c76b3bd0ff0ac923d43c35
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-05 16:48:00 +0900

    finish task11_2 Asset

--- src/Asset.java
+++ src/Asset.java
@@ -1,4 +1,11 @@
 
-public class Asset {
-
+public abstract class Asset {
+	String name;
+	int price;
+	public Asset(String name, int price) {
+		this.name = name;
+		this.price = price;
+	}
+	public String getName() { return this.name; }
+	public int getPrice() { return this.price; }
 }
