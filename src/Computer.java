2a22ca6e1dcbef18e9eec43edfd9ad1536f16c88
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-05 16:35:27 +0900

    finish Computer

--- src/Computer.java
+++ src/Computer.java
@@ -1,4 +1,17 @@
 
 public class Computer {
-	
+	String name;
+	int price;
+	String color;
+	String makerName;
+	public Computer(String name, int price, String color, String makerName) {
+		this.name = name;
+		this.price = price;
+		this.color = color;
+		this.makerName = makerName;
+	}
+	public String getName() { return this.name; }
+	public int getPrice() { return this.price; }
+	public String getColor() { return this.color; }
+	public String getmakerName() { return this.makerName; }
 }
