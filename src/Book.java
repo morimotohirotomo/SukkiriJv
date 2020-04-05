b1e127c827a9966f3e3d6ad1ae498c098090f31e
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-05 16:43:34 +0900

    finish task11_1

--- src/Book.java
+++ src/Book.java
@@ -1,17 +1,9 @@
 
-public class Book {
-	String name;
-	int price;
-	String color;
+public class Book extends TangibleAsset {
 	String isbn;
 	public Book(String name, int price, String color, String isbn) {
-		this.name = name;
-		this.price = price;
-		this.color = color;
+		super(name, price, color);
 		this.isbn = isbn;
 	}
-	public String getName() { return this.name; }
-	public int getPrice() { return this.price; }
-	public String getColor() { return this.color; }
 	public String getIsbn() { return this.isbn; }
 }
