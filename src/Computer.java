02574329462e626fdae8c897a524bd260fc5099c
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-05 16:41:52 +0900

    finish task11_1 Computer

--- src/Computer.java
+++ src/Computer.java
@@ -1,17 +1,12 @@
 
-public class Computer {
+public class Computer extends TangibleAsset{
 	String name;
 	int price;
 	String color;
 	String makerName;
 	public Computer(String name, int price, String color, String makerName) {
-		this.name = name;
-		this.price = price;
-		this.color = color;
+		super(name, price, color);
 		this.makerName = makerName;
 	}
-	public String getName() { return this.name; }
-	public int getPrice() { return this.price; }
-	public String getColor() { return this.color; }
 	public String getmakerName() { return this.makerName; }
 }
