a9c4f0c7726f17711fa872c3b0721e8d05af5120
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-07 18:10:02 +0900

    finish task12_3

--- src/AB.java
+++ src/AB.java
@@ -1,6 +1,11 @@
 
 public class AB {
 	public static void main(String[] args) {
-
+		Y[] array = new Y[2];
+		array[0] = new A();
+		array[1] = new B();
+		for (Y y : array) {
+			y.b();
+		}
 	}
 }
