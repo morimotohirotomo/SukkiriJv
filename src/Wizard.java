a434c06bb8409e0b9712b669eb26b7df8c4d64cf
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-07 21:51:04 +0900

    finish task13_4

--- src/Wizard.java
+++ src/Wizard.java
@@ -11,9 +11,21 @@ public class Wizard {
 		System.out.println(h.getName() + "のHPを" + recoverPoint + "回復した！");
 	}
 	public int getHp() {return this.hp;}
-	public void setHp(int hp) {this.hp = hp;}
+	public void setHp(int hp) {
+		if(hp < 0) {
+			this.hp = 0;
+		} else {
+			this.hp = hp;
+		}
+	}
 	public int getMp() {return this.mp;}
-	public void setMp(int mp) {this.mp = mp;}
+	public void setMp(int mp) {
+		if(mp < 0) {
+			throw new IllegalArgumentException
+			("mp0以上");
+		}
+		this.mp = mp;
+	}
 	public String getName() {return this.name;}
 	public void setName(String name) {
 		if(name == null || name.length() < 3) {
@@ -23,5 +35,11 @@ public class Wizard {
 		this.name = name;
 	}
 	public Wand getWand() {return this.wand;}
-	public void setWand(Wand wand) {this.wand = wand;}
+	public void setWand(Wand wand) {
+		if(wand == null) {
+			throw new IllegalArgumentException
+			("名前を入力してください");
+		}
+		this.wand = wand;
+	}
 }
