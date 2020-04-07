f4279b124a3fa53df239b5feff9b3f38b4dfd253
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-07 21:29:44 +0900

    finish task13_3

--- src/Wizard.java
+++ src/Wizard.java
@@ -6,8 +6,16 @@ public class Wizard {
 	private Wand wand;
 	public void heal(Hero h) {
 		int basePoint = 10;
-		int recoverPoint = (int)(basePoint * this.wand.power);
+		int recoverPoint = (int)(basePoint * this.getWand.getPower);
 		h.setHp(h.getHp() + recoverPoint);
 		System.out.println(h.getName() + "のHPを" + recoverPoint + "回復した！");
 	}
+	public int getHp() {return this.hp;}
+	public void setHp(int hp) {this.hp = hp;}
+	public int getMp() {return this.mp;}
+	public void setMp(int mp) {this.mp = mp;}
+	public String getName() {return this.name;}
+	public void setName(String name) {this.name = name;}
+	public Wand getWand() {return this.wand;}
+	public void setWand(Wand wand) {this.wand = wand;}
 }
