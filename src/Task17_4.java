9803e7c0a8d42735574167d6f6af00b6c0ef28b1
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-14 18:14:02 +0900

    finish task17_4

---
+++ src/Task17_4.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class Task17_4 {
+	public static void main(String[] args) throws IOException {
+		System.out.println("プログラムが起動しました");
+		throw new IOException();
+	}
+}
