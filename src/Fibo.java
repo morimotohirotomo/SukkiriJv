be16956be13f7f041b09bd0a8ef688f8699d6869
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-16 17:50:08 +0900

    finish fibo

--- src/Fibo.java
+++ src/Fibo.java
@@ -1,4 +1,18 @@
-
+import java.util.Scanner;
 public class Fibo {
-
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("1以上の整数を入力してください");
+		int input = scanner.nextInt();
+		for (int n = 0; n < input; n++) {
+			System.out.println(fibo(n));
+		}
+	}
+	public static int fibo(int num) {
+		if (num < 2) {
+			return num;
+		} else {
+			return fibo(num - 1) + fibo(num - 2);
+		}
+	}
 }
