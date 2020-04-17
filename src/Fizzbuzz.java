53c448da05ff500b7ebd6108c61e2a85ac7ac048
morimotohirotomo <minamoyori19@outlook.jp>
2020-04-17 16:50:49 +0900

    modify fibo

--- src/Fizzbuzz.java
+++ src/Fizzbuzz.java
@@ -3,20 +3,22 @@ public class Fizzbuzz {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int input = scanner.nextInt();
-	    for(int x = 1; x <= input; x++) {
-		    fizzbuzz(x);
+	    for(int i = 1; i <= input; i++) {
+	    	System.out.println(
+	    		((i % 15 == 0) ? "FizzBuzz" : ((i % 5 == 0) ? "Buzz" : ((i % 3 == 0) ? "Fizz" : i)))
+	    	);
 		}
 	}
 
-	public static void fizzbuzz(int n) {
-		if (n % 15 == 0) {
-			System.out.println("fizzbuzz");
-		} else if (n % 3 == 0) {
-			System.out.println("fizz");
-		} else if (n % 5 == 0) {
-			System.out.println("buzz");
-		} else {
-			System.out.println (n);
-		}
-	}
+//	public static void fizzbuzz(int n) {
+//		if (n % 15 == 0) {
+//			System.out.println("fizzbuzz");
+//		} else if (n % 3 == 0) {
+//			System.out.println("fizz");
+//		} else if (n % 5 == 0) {
+//			System.out.println("buzz");
+//		} else {
+//			System.out.println (n);
+//		}
+//	}
 }
